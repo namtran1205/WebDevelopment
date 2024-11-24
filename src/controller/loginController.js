4f90c4433fd62ebdf4ef6bb1359027e7159f9931
Hoàng Túy Minh <tuyminh.hoang@gmail.com>
2024-11-24 18:42:44 +0700

    use bcrypt to encrypt password
    changes:
    - [controller/signUpController.js] added bcrypt hashing
    - [controller/signUpController.js]
      + changed to using email as identifier
      + added password check with bcrypt
    - [views/login.ejs] changed label and placeholder to display email for login

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -1,17 +1,18 @@
 const { render } = require('ejs');
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 const loginController = 
 {
     async loginUser (req, res)
     {
-        const { name, password } = req.body;
+        const { mail, password } = req.body;
 
         try 
         {
-            const record = await User.findOne({ fullName: name }); //// add password field when available
+            const record = await User.findOne({ email: mail });
             console.log(record); //// debug
-            if (record === null)
+            if (record === null) // mail does not exist
             {
                 res.locals.parameters = 
                 {
@@ -19,34 +20,52 @@ const loginController =
                 };
                 res.render('login');
             }
-            else 
+            
+            bcrypt.compare(password, record.password, async function (err, result)
             {
-                // temporary
-                switch (record.type) {
-                    case 'subscriber':
-                        res.send('subscriber page');
-                        break;
-                
-                    case 'writer':
-                        res.send('writer page');
-                        break;
-
-                    case 'editor':
-                        res.send('editor page');
-                        break;
-
-                    case 'admin':
-                        res.send('admin page');
-                        break;
-                        
-                    default:
-                        console.log('unknown user type');
-                        res.redirect('/');
-                        break;
+                if (err)
+                {
+                    console.error(err);
+                    res.render('login');
+                    return;
                 }
-            }
+
+                if (result) // correct password
+                {
+                    switch (record.type) {
+                        case 'subscriber':
+                            res.send('subscriber page');
+                            break;
+                    
+                        case 'writer':
+                            res.send('writer page');
+                            break;
+        
+                        case 'editor':
+                            res.send('editor page');
+                            break;
+        
+                        case 'admin':
+                            res.send('admin page');
+                            break;
+                            
+                        default:
+                            console.log('unknown user type');
+                            res.redirect('/');
+                            break;
+                    }
+                }
+                else // incorrect password
+                {
+                    res.locals.parameters = 
+                    {
+                        failure: true
+                    };
+                    res.render('login');
+                }
+            })
         } catch (error) {
-            console.log(error);
+            console.error(error);
             res.render('login');
         }
     }
--- src/controller/signUpController.js
+++ src/controller/signUpController.js
@@ -1,6 +1,7 @@
 const { render } = require('ejs');
 const path = require('path');
 const fs = require('fs');
+const bcrypt = require('bcrypt');
 const User = require("../models/User");
 
 class signUpController {
@@ -16,19 +17,30 @@ class signUpController {
   async createUser(req, res) {
     try {
       const { fullName, password, nickname, email, dateOfBirth, type } = req.body;
-  
-      const newUser = new User({
-        fullName,
-        password,
-        nickname: type === "writer" ? nickname : null,
-        email,
-        dateOfBirth,
-        type,
+
+      const saltRounds = 10;
+      bcrypt.hash(password, saltRounds, async function(err, hash) {
+        if (err)
+        {
+          console.error(err);
+          res.redirect(`/api/v1/signup?error=true&message=${encodeURIComponent(error.message)}`);
+          return;
+        }
+
+        const newUser = new User({
+          fullName,
+          password: hash,
+          nickname: type === "writer" ? nickname : null,
+          email,
+          dateOfBirth,
+          type,
+        });
+        
+        await newUser.save();
+
+        res.redirect('/api/v1/signup?success=true');
       });
   
-      await newUser.save();
-  
-      res.redirect('/api/v1/signup?success=true');
     } catch (error) {
       console.error("Error signing up user:", error.message);
   
