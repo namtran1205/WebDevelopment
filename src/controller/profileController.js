a64e26bf672fc0bad7b6406fd43ea1516b85544c
namtran1205 <nguyenhanamtranhv1205@gmail.com>
2024-11-28 10:13:39 +0700

    update userType in profile

--- src/controller/profileController.js
+++ src/controller/profileController.js
@@ -7,27 +7,32 @@ const profileController =
     async show(req, res) {
         let user = null;
         let userId = null;
+        let userType = null;
 
         switch (true) {
             case !!req.cookies.subscriber:
                 user = JSON.parse(req.cookies.subscriber);
                 userId = user._id;
+                userType = 'Độc giả';
                 break;
             case !!req.cookies.admin:
                 user = JSON.parse(req.cookies.admin);
                 userId = user._id;
+                userType = 'Quản trị viên';
                 break;
             case !!req.cookies.writer:
                 user = JSON.parse(req.cookies.writer);
                 userId = user._id;
+                userType = 'Phóng viên';
                 break;
             case !!req.cookies.editor:
                 user = JSON.parse(req.cookies.editor);
                 userId = user._id;
+                userType = 'Biên tập viên';
                 break;
         }
 
-        res.render('profile', { userId, user });
+        res.render('profile', { userId, userType, user });
     },
 
     async update(req, res)
@@ -39,20 +44,25 @@ const profileController =
             case !!req.cookies.subscriber:
                 user = JSON.parse(req.cookies.subscriber);
                 userId = user._id;
+                
                 break;
             case !!req.cookies.admin:
                 user = JSON.parse(req.cookies.admin);
                 userId = user._id;
+                
                 break;
             case !!req.cookies.writer:
                 user = JSON.parse(req.cookies.writer);
                 userId = user._id;
+                
                 break;
             case !!req.cookies.editor:
                 user = JSON.parse(req.cookies.editor);
                 userId = user._id;
+                
                 break;
         }
+    
         res.render('profile', { userId, user });
     }
 };
